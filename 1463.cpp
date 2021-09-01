3e3a2edf6278e5dd3432c1517c2c6345846ffd3d
y-siro <82633123+y-siro@users.noreply.github.com>
2021-09-01 09:11:58 +0900

    DP

---
+++ 1463.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL)
+#define pb push_back
+#define mp make_pair
+#define vc vector
+#define vi vector<int>
+#define vl vector<ll>
+#define pq priority_queue<int,vector<int>,less<int>>
+#define pql priority_queue<int,vector<int>,greater<int>>
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define all(v) v.begin(), v.end()
+#define srt(v) sort(all(v))
+#define F first
+#define S second
+#define endl "\n"
+typedef long long ll;
+
+int n, d[100001];
+int main(void) {
+	fast;
+
+	cin >> n;
+	d[1] = 0;
+	for (int i = 2; i <= n; ++i) {
+		d[i] = d[i - 1] + 1;
+		if (i % 2 == 0) d[i] = min(d[i], d[i / 2] + 1);
+		if (i % 3 == 0) d[i] = min(d[i], d[i / 3] + 1);
+	}
+	cout << d[n];
+	return 0;	
+}

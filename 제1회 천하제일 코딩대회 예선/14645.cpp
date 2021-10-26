f7068233865e7da8dd1d3b3ae86c37f169d4a210
y-siro <82633123+y-siro@users.noreply.github.com>
2021-10-27 01:35:44 +0900

    구현

---
+++ 제1회 천하제일 코딩대회 예선/14645.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL)
+using ll = long long;
+
+// boj 14645 와이버스 부릉부릉 - 구현 
+
+int N, K, num;
+int main(void) {
+	fast;
+	cin >> N >> K;
+	for (int i = 0; i < N * K; ++i) cin >> num;
+	cout << "비와이";
+}

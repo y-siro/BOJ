5719dfbcddd880fee5d09ec292bb50bfe3cec8a0
y-siro <82633123+y-siro@users.noreply.github.com>
2021-10-30 23:50:42 +0900

    구현

---
+++ 제3회 천하제일 코딩대회 예선/17295(A).cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL)
+using ll = long long;
+
+// boj 17295 엔드게임 스포일러 - 구현
+
+int main(void) {
+	cout << "Avengers: Endgame";
+}

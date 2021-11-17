d9d2e3be408961d3504a3c875b439695c08e80d2
y-siro <82633123+y-siro@users.noreply.github.com>
2021-11-17 09:58:36 +0900

    그래프 이론, 트리

---
+++ 9372.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL)
+
+// 9372 상근이의 여행 - 그래프 이론, 트리
+
+int T,N,M;
+int main(void){
+    fast;
+    cin>>T;
+    while(T--){
+        cin>>N>>M;
+        for(int i=0;i<M;++i){
+            int s,e; cin>>s>>e;
+        }
+        cout<<N-1<<"\n";
+    }
+}
+        

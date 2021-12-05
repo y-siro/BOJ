93db4df75ccb4981329e27cca8e2c3d4570b6d3c
y-siro <82633123+y-siro@users.noreply.github.com>
2021-12-05 19:31:28 +0900

    수학, 구현, 사칙연산

--- 2021 아주대학교 프로그래밍 경시대회 APC Open Contest/A.cpp
+++ 2021 아주대학교 프로그래밍 경시대회 APC Open Contest/A.cpp
@@ -1,11 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL)
+#define vc vector
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 using ll = long long;
 
 // boj 2021 아주대학교 프로그래밍 경시대회 APC Open Contest - A
 
-string s,str,ms="99999999"; int N,mx=0,Y=0,M=0,D=0;
+string s, str, ms = "99999999"; int N, mx = 0, Y = 0, M = 0, D = 0;
 
 int main(void) {
 
@@ -18,10 +23,12 @@ int main(void) {
 		for (int j = 6; j < 8; ++j) D += pow(s[j] - str[j], 2);
 		Y *= M * D;
 
-		if (mx <= Y) {		
-			int cur = stoi(str); int res = stoi(ms);
-			if(cur<res)	ms = str;
+		if (Y>mx) {
 			mx = Y;
+			ms = str;
+		}
+		if (Y == mx) {
+			if (ms > str) ms = str;
 		}
 		Y = 0; M = 0; D = 0;
 	}

54497cb556b6b883cf3e0a3c3b4813adb6f00545
y-siro <82633123+y-siro@users.noreply.github.com>
2021-12-22 08:45:48 +0900

    게임 이론

---
+++ 9656.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL)
+#define ll long long
+#define vc vector
+#define vi vector<int>
+#define vs vector<string>
+#define pb push_back
+#define mp make_pair
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+int main(void){
+    fast;
+    int N; cin>>N;
+    if(N%2) cout<<"CY";
+    else cout<<"SK";
+}

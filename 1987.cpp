1de5b9a54f94636b096d404dc9c89b62b672976b
y-siro <82633123+y-siro@users.noreply.github.com>
2022-01-21 16:58:29 +0900

    Backtracking, dfs

--- 1987.cpp
+++ 1987.cpp
@@ -1,54 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define fast ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL)
-#define pb push_back
-#define mp make_pair
+#define ll long long
 #define vc vector
 #define vi vector<int>
-#define vl vector<ll>
-#define pq priority_queue<int,vector<int>,less<int>>
-#define pql priority_queue<int,vector<int>,greater<int>>
-#define pi pair<int,int>
-#define pr pair<ll,ll>
-#define all(v) v.begin(), v.end()
-#define srt(v) sort(all(v))
-#define F first
-#define S second
-#define endl "\n"
-typedef long long int ll;
+#define vs vector<string>
+#define pb push_back
+#define pq priority_queue<int>
+#define pql priority_queue<int, vector<int>, greater<> >
+#define pii pair<int,int>
+#define pll pair<ll,ll>
 
-const int M = 21;
-int r, c, cnt = 0, max_val=0;
-string str; vector<string> s;
-int visited[26] = { false, };
-int d[4][2] = { {0,1},{0,-1},{-1,0},{1,0} };
-void dfs(int x, int y,int cnt) {
-	max_val = max(max_val, cnt);
-	for (int i = 0; i < 4; ++i) {
-		int nx = x + d[i][0];
-		int ny = y + d[i][1];
-		if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-		int idx = (int)s[nx][ny] - 65;
-		if (!visited[idx]) {
-			visited[idx]++;
-			dfs(nx, ny,cnt+1);
-			visited[idx]--;
-		}
-	}
+int R,C,mm=0, cnt=0;
+vector<string> A;
+int d[4][2]={{1,0},{-1,0},{0,-1},{0,1}};
+bool alp[26];
+
+void dfs(int x, int y){
+    cnt++;
+    mm=max(mm,cnt);
+    alp[A[x][y]-'A']=true;
+    for(int i=0;i<4;++i){
+        int nx=x+d[i][0], ny=y+d[i][1];
+        if(nx<0 or ny<0 or nx>=R or ny>=C) continue;
+        if(alp[A[nx][ny]-'A']) continue;
+        alp[A[nx][ny]-'A']=true;
+        dfs(nx, ny);
+        alp[A[nx][ny]-'A']=false; cnt--;
+    }
 }
 
-int main(void) {
-	fast;
-
-	cin >> r >> c;
-	for (int i = 0; i < r; ++i) {
-		cin >> str;
-		s.pb(str);
-	}
-	int idx = (int)s[0][0] - 65;
-	visited[idx]++;
-	dfs(0, 0, 1);
-	cout << max_val;
-
-	return 0;
+int main(void){
+    fast;
+    cin>>R>>C;
+    for(int i=0;i<R;++i){
+        string s; cin>>s;
+        A.push_back(s);
+    }
+    dfs(0,0);
+    cout<<mm;
 }
+
+
